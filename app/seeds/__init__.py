63b4483da214c5728fb03c705f415d1c16db3410
Clint Hill <clint.hill07@gmail.com>
2021-03-05 12:53:38 -0500

    Seed topics.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .stickers import seed_stickers, undo_stickers
+from .topics import seed_topics, undo_topics
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -11,6 +12,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_stickers()
     seed_users()
+    seed_topics()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
@@ -18,4 +20,5 @@ def seed():
 def undo():
     undo_users()
     undo_stickers()
+    undo_topics()
     # Add other undo functions here
