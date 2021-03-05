e9d7122527f83985e1624e09d26bb60f75abbcd8
Clint Hill <clint.hill07@gmail.com>
2021-03-05 12:14:32 -0500

    Sticker seed.

--- react-app/src/components/ConversationPage/ConversationPage.js
+++ react-app/src/components/ConversationPage/ConversationPage.js
@@ -17,7 +17,8 @@ const ChatComponent = styled.div`
   height: 70%;
   display: flex;
   margin-top: 30px;
-  box-shadow: 8px 8px 8px gray;
+  border-radius: 10px 10px 0 0;
+  box-shadow: 2px 2px 5px gray;
 `;
 
 const ChatList = styled.div`
