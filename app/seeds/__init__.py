d99e00c7055ef470a6b5dd376f40854647205eb9
Clint Hill <clint.hill07@gmail.com>
2021-03-04 22:19:09 -0500

    Add stickers

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .stickers import seed_stickers, undo_stickers
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -8,6 +9,7 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    seed_stickers()
     seed_users()
     # Add other seed functions here
 
@@ -15,4 +17,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_stickers()
     # Add other undo functions here
